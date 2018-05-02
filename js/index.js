35e9d57f14e27a90ef9574e541b4ebfa16f7ee75
Tushar mahajan <tushar.mahajan37@gmail.com>
2018-05-03 02:51:13 +0530

    Add a timer to the landing page

--- js/index.js
+++ js/index.js
@@ -1,38 +1,29 @@
-const countdown = document.querySelector('.countdown');
+// Due    
+var dueDate = "May 17 2018 23:59:00 UTC+10"
 
-// Set Launch Date (ms)
-const launchDate = new Date('Jan 1, 2019 13:00:00').getTime();
-
-// Update every second
-const intvl = setInterval(() => {
-  // Get todays date and time (ms)
-  const now = new Date().getTime();
-
-  // Distance from now and the launch date (ms)
-  const distance = launchDate - now;
-
-  // Time calculation
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display result
-  countdown.innerHTML = `
-  <div>${days}<span>Days</span></div> 
-  <div>${hours}<span>Hours</span></div>
-  <div>${mins}<span>Minutes</span></div>
-  <div>${seconds}<span>Seconds</span></div>
-  `;
-
-  // If launch date is reached
-  if (distance < 0) {
-    // Stop countdown
-    clearInterval(intvl);
-    // Style and output text
-    countdown.style.color = '#17a2b8';
-    countdown.innerHTML = 'Launched!';
-  }
+// Countdown (1sec)
+var timer = setInterval(function() {
+    var closingTime = new Date(dueDate).getTime();
+    var currentTime = new Date().getTime();
+    var interval = closingTime - currentTime;
+    
+    // Calculate
+    var days = Math.floor(interval / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((interval % (1000 * 60)) / 1000);
+    
+    // Output
+    document.getElementById("timerCounter").innerHTML = 
+        "<span class='counter-day'>"    + "<strong>" + days    + "</strong>" + "days"    + "</span> " + 
+        "<span class='counter-hour'>"   + "<strong>" + hours   + "</strong>" + "hours"   + "</span> " + 
+        "<span class='counter-minute'>" + "<strong>" + minutes + "</strong>" + "minutes" + "</span> " +  
+        "<span class='counter-second'>" + "<strong>" + seconds + "</strong>" + "seconds" + "</span>";
+    
+    // Expired
+    if (interval < 0) {
+        clearInterval(timer); // Stop timer
+        document.getElementById("timerTitle").innerHTML = "Voting closed";
+        document.getElementById("timerCounter").innerHTML = "";
+    }
 }, 1000);
